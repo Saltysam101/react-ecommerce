03122927dd815ddd8ff408b7add00b53962b6f2f
Sam <s.zudell98@gmail.com>
2022-08-23 11:09:51 -0400

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+    return ( 
+    <div className = "App" >
+      Hello world client
+
+    </div>
+    );
+}
+
+export default App;
---
+++ server/server.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+
+app.get('/', (req, res) => {
+    res.send('Hello world server')
+})
+
+
+app.listen(PORT, () => {
+    console.log(`The server is listening on port ${PORT}`)
+})
